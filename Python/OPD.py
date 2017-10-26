7d466b4d3eb017c5f04043d94e7f6c68c81baeb2
Jojo <jordan.voirin@gmail.com>
2017-10-26 10:06:46 +0200

    Create script to plot model of astigmatism

--- Python/OPD.py
+++ Python/OPD.py
@@ -1,22 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Éditeur de Spyder
+
+Ceci est un script temporaire.
+"""
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
-sns.set()
 
+def abberationAstigmatism(n,e,up,N):
+    return (n**2-1)*e*up**2/(2*n**3*N)
+    
+n = 1.49
+e = 1.4e-2
+phi = np.linspace(0,60,13)
+theta = 90-phi
+beta = 90-theta
+pup = 3.2e-3
+f = 200e-3
+N = f/pup
+alphamax = np.arctan(pup/f)
+alphamin = np.arctan(-pup/f)
+upmax = (beta+alphamax)*np.pi/180
+upmin = (beta+alphamin)*np.pi/180
 
-angles = np.array([0., 5., 10., 15., 20., 25., 30., 35., 40., 45., 50., 55.])
-anglesRad = angles/180*np.pi
-
-nAir = 1.000293
-nPlexi = 1.49
-
-d = 0.01 #[m]
-
-ds = d/np.cos(anglesRad)
-
-OPD = ds*nPlexi-d*nAir
+OPDmaxmin = abberationAstigmatism(n,e,upmax,N)-abberationAstigmatism(n,e,upmin,N)
 
 plt.figure()
-plt.plot(angles, OPD*1000)
-plt.xlabel('Angle [$^{\circ}$]')
-plt.ylabel('OPD [mm]')
+plt.plot(phi,OPDmaxmin*1e9,'b-2')
+plt.xlabel('Plane Parallel Plate Angle [$^{\circ}$]')
+plt.ylabel('P2V OPD [nm]')
