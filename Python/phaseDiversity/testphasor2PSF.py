f7bad1ca29cef8f852c4bb58d8f20a9feb424841
Jojo <jordan.voirin@gmail.com>
2017-12-12 11:20:41 +0100

    UPDT

--- Python/phaseDiversity/testphasor2PSF.py
+++ Python/phaseDiversity/testphasor2PSF.py
@@ -2,7 +2,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 from libtim import all
 
+%matplotlib inline
+%config 
+
 rad = int(np.ceil(pupilRadius/dxp))
 grid_mask = (im.mk_rad_mask(2*rad)) <= 1
 pupil = np.ones((2*rad,2*rad))*grid_mask
-FFTPupil = np.fft.fftshift(np.fft.fft2(pupil))
+matpupil = np.zeros((4*rad,4*rad))
+matpupil[rad:3*rad,rad:3*rad] = pupil
+FFTPupil = np.fft.fftshift(np.fft.fft2(matpupil))
+
+NFFTPupil = np.abs(FFTPupil)**2/np.sum(pupil)
+
+plt.figure()
+plt.imshow(matpupil)
+
+plt.figure()
+plt.imshow(NFFTpupil,vmax=0.,vmin=2.)
