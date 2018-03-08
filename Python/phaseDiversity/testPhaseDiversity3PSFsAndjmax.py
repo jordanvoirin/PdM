2bfae7ec2a4a175d23895bfbe526b7afd462e049
Jojo <jordan.voirin@gmail.com>
2018-03-08 11:28:44 +0100

    UPDT

--- Python/phaseDiversity/testPhaseDiversity3PSFsAndjmax.py
+++ Python/phaseDiversity/testPhaseDiversity3PSFsAndjmax.py
@@ -16,16 +16,16 @@ N = 400
 dxp = lbda*F/(N*pxsize)
 deltaZ = 3.19e-3
 jmin = 4
-jmax = 30
+jmax = np.array([10,20,30,50,100,150,200])
 rmsWFerror = 30.
 noiseStdLevel = 0.001
 
 P2Vdephasing = np.pi*deltaZ/lbda*(2*pupilRadius/F)**2/4.
 a4dephasing = P2Vdephasing/2/np.sqrt(3)
 
-js = np.linspace(jmin,jmax,num=jmax-3)
+js = np.linspace(jmin,jmax[-1],num=jmax[-1]-3)
 
-jscomplete = np.linspace(jmin,jmax,num=jmax-3)
+jscomplete = np.linspace(jmin,jmax[-1],num=jmax[-1]-3)
 ajscomplete = jscomplete*.0
 
 ajstrue = fsApd.getRandomAjs(js,rmsWFerror)*1e-9/lbda*2*np.pi
@@ -65,19 +65,20 @@ PSFoutfocneg = psf.PSF(jswth4,ajswtha4neg,N,dxp,pupilRadius)
 #noiseStd = np.max(PSFoutfocneg.PSF)*noiseStdLevel
 #whiteNoise = fsApd.generateWhiteNoise((PSFoutfocneg.PSF).shape,noiseMean,noiseStd)
 #PSFoutfocnegWthNoise = PSFoutfocneg.PSF+whiteNoise
-
-phaseDivWoutNoise = PD.phaseDiversity3PSFs(PSFinfoc.PSF,PSFoutfocpos.PSF,PSFoutfocneg.PSF,deltaZ,lbda,pxsize,F,pupilRadius,jmin,jmax)
+phaseDivWoutNoise = np.array([])
+for jm in jmax:
+    phaseDivWoutNoise = np.hstack((phaseDivWoutNoise,PD.phaseDiversity3PSFs(PSFinfoc.PSF,PSFoutfocpos.PSF,PSFoutfocneg.PSF,deltaZ,lbda,pxsize,F,pupilRadius,jmin,jm)))
 #phaseDivWthNoise = PD.phaseDiversity3PSFs(PSFinfocWthNoise,PSFoutfocposWthNoise,PSFoutfocnegWthNoise,deltaZ,lbda,pxsize,F,pupilRadius,jmin,jmax)
 #print phaseDiv.result['ajs']*1e9*lbda/2/np.pi
 #print ajstrue*1e9*lbda/2/np.pi
 
 plt.figure()
 plt.hold(True)
-plt.plot(jscomplete,ajscomplete*1e9*lbda/2/np.pi,'b-',label='true, $\sigma_{WF,rms}$ = %5.3f nm' %(fs.RMSwavefrontError(js,ajstrue*1e9*lbda/2/np.pi)))
-plt.errorbar(phaseDivWoutNoise.result['js'],phaseDivWoutNoise.result['ajs']*1e9*lbda/2/np.pi,yerr=phaseDivWoutNoise.result['ajsSte']*1e9*lbda/2/np.pi
-    ,color = 'red',lineStyle='-',label='retrieved wout noise, $\sigma_{WF,rms}$ = %5.3f nm, RMSE = %5.3f nm'
-    %(fs.RMSwavefrontError(phaseDivWoutNoise.result['js'],phaseDivWoutNoise.result['ajs']*1e9*lbda/2/np.pi)
-    ,fs.RMSE(phaseDivWoutNoise.result['ajs']*1e9*lbda/2/np.pi,ajscomplete*1e9*lbda/2/np.pi)))
+plt.plot(jscomplete,ajscomplete*1e9*lbda/2/np.pi,'b-',linewidth=2,label='true')
+for ij,jm in enumerate(jmax): 
+    plt.errorbar(phaseDivWoutNoise[ij].result['js'],phaseDivWoutNoise[ij].result['ajs']*1e9*lbda/2/np.pi,yerr=phaseDivWoutNoise[ij].result['ajsSte']*1e9*lbda/2/np.pi
+        ,lineStyle='-',label='retrieved jmax = %d, RMSE = %5.3f nm'
+        %(jm,fs.RMSE(phaseDivWoutNoise[ij].result['ajs']*1e9*lbda/2/np.pi,ajscomplete[0:jm-3]*1e9*lbda/2/np.pi)))
 #plt.errorbar(phaseDivWthNoise.result['js'],phaseDivWthNoise.result['ajs']*1e9*lbda/2/np.pi,yerr=phaseDivWthNoise.result['ajsSte']*1e9*lbda/2/np.pi
 #    ,color = 'green',lineStyle='-',label='retrieved wth noise, $\sigma_{WF,rms}$ = %5.3f nm, RMSE = %5.3f nm'
 #    %(fs.RMSwavefrontError(phaseDivWthNoise.result['js'],phaseDivWthNoise.result['ajs']*1e9*lbda/2/np.pi)
@@ -88,9 +89,9 @@ plt.xlim([jscomplete[0],jscomplete[-1]])
 plt.legend(loc='best')
 plt.grid()
 
-fnameajsjs = '../../../fig/PDDev/test/ajs_js_jmax_%d_WoutN_rmsWFe_%d%s'
-plt.savefig(fnameajsjs % (jmax,rmsWFerror,'.png'), dpi=300)
-plt.savefig(fnameajsjs % (jmax,rmsWFerror,'.pdf'), dpi=300)
+fnameajsjs = '../../../fig/PDDev/test/ajs_js_jmaxstudy_WoutN_rmsWFe_%d%s'
+plt.savefig(fnameajsjs % (rmsWFerror,'.png'), dpi=300)
+plt.savefig(fnameajsjs % (rmsWFerror,'.pdf'), dpi=300)
 
 plt.figure()
 plt.subplot(2,3,4)
@@ -172,6 +173,6 @@ plt.tick_params(
     labelright='off',
     labelleft='off')
 plt.show()
-fnameajsjs = '../../../fig/PDDev/test/PhasesAndPSFs_jmax_%d_WoutN_rmsWFe_%d%s'
-plt.savefig(fnameajsjs % (jmax,rmsWFerror,'.png'), dpi=300)
-plt.savefig(fnameajsjs % (jmax,rmsWFerror,'.pdf'), dpi=300)
+fnameajsjs = '../../../fig/PDDev/test/PSFs_jmaxstudy_WoutN_rmsWFe_%d%s'
+plt.savefig(fnameajsjs % (rmsWFerror,'.png'), dpi=300)
+plt.savefig(fnameajsjs % (rmsWFerror,'.pdf'), dpi=300)
